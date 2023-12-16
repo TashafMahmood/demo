2c4975c9aa780e8ec1632588593288f93211b9fd
TashafMahmood <tashaf@elred.io>
2023-12-16 12:39:57 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './TASHAFTECH.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
